4620fe49c4d6a405fbdd6eec231b5ee8c386d3fb
Muhammad Talha Siddiqui <64930136+M-Talha-Siddiqui@users.noreply.github.com>
2020-07-31 22:17:03 +0200

    update

--- tests/test_intersect.py
+++ tests/test_intersect.py
@@ -20,7 +20,7 @@ def test__intersect_ray_with_sphere__two_points__return_true():
     assert np.array_equal(_intersect_ray_with_sphere(Ray((-2,0,0),(1,0,0)),Sphere((0,0,0),1.0)),[Point([1,0,0]),Point([-1,0,0])]) is True
 
 
-def test__intersect_ray_with_sphere__ray_in_sphere__one_point__return_true():
+def test__intersect_ray_with_sphere__one_point__return_true():
     assert (_intersect_ray_with_sphere(Ray((0,0,1),(0,1,0)),Sphere((0,0,0),1.0))==Point([0,0,1]))
 
 def test__intersect_ray_with_sphere__ray_outside_sphere__one_point__return_true():
@@ -30,7 +30,7 @@ def test__intersect_ray_with_sphere__ray_outside_sphere__one_point__return_true(
 def test__intersect_ray_with_sphere__no_points__return_true():
     assert (_intersect_ray_with_sphere(Ray((-1.5,1.5,0),(1,0,0)),Sphere((0,0,0),1.0)) == None)
 
-def test__intersect_ray_with_sphere__ray_outside_sphere__two_points__return_true():
+def test__ray_outside_sphere__two_points__return_true():
     assert np.array_equal(_intersect_ray_with_sphere(Ray((-2,1,1),(1,0,0)),Sphere((1,1,1),1.0)),[Point([2,1,1]),Point([0,1,1])]) is True
 
 
